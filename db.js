f9486d5e0d9cbc08f817bd260215bbb2ad498612
maxisthemax <maxisthemax89@gmail.com>
2019-06-24 23:45:31 +0800

    fix

--- db.js
+++ db.js
@@ -13,5 +13,24 @@ con.connect(function (err) {
 
     console.log('connected as id ' + con.threadId);
 });
+handleDisconnect(con);
+
+function handleDisconnect(conn) {
+    con.on('error', function (err) {
+        if (!err.fatal) {
+            return;
+        }
+
+        if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+            throw err;
+        }
+        console.log('Re-connecting lost connection: ' + err.stack);
+        conn = mysqlDriver.createConnection(config.CLEARDB_DATABASE_URL);
+        handleDisconnect(con);
+        conn.connect();
+    });
+}
+handleDisconnect(con);
 
 module.exports = con;
+
