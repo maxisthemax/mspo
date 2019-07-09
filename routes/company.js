69e41f4881b568f30527973e07973eafb03215d4
maxisthemax <maxisthemax89@gmail.com>
2019-07-10 00:08:48 +0800

    fix

--- routes/company.js
+++ routes/company.js
@@ -11,28 +11,6 @@ module.exports = {
             });
         });
     },
-    createCompany: (req, res) => {
-        comp.company.createCompany(req, function(err, company) {
-            //console.log(customer);
-            if (err) {
-                if (err.code == "ER_DUP_ENTRY")
-                    req.flash('error', 'Company Already Exist');
-                else
-                    req.flash('error', 'Unable to Save Company Data');
-            } else if (company) {
-
-                var defaultfolder = `public/company/${company.insertId}/`;
-                if (!fs.existsSync(defaultfolder)) {
-                    mkdirp(defaultfolder, function(err) {
-                        if (err) console.error(err)
-                        else console.log('dir created')
-                    });
-                }
-                req.flash('success', 'New Compnay Created');
-            }
-            res.redirect('/company/');
-        });
-    },
     saveCompany: (req, res) => {
         //console.log(req.body);
         comp.company.saveCompany(req.body, function(err, company) {
