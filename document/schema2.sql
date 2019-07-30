7cfde8d831920c8593e18cda8e8fc81ab59056eb
maxisthemax <maxisthemax89@gmail.com>
2019-07-30 09:09:45 +0800

    fix

--- document/schema2.sql
+++ document/schema2.sql
@@ -1,1 +1,2 @@
+ALTER TABLE `mpobs` ADD `mspoId` INT NOT NULL AFTER `landId`;
 ALTER TABLE `lands` ADD `mukim` VARCHAR(200) NOT NULL AFTER `disabled`, ADD `yearPlanted` INT NOT NULL AFTER `mukim`, ADD `isMSPO` SMALLINT NOT NULL AFTER `yearPlanted`;
