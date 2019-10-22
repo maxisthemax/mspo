ed46fab97d037488656984a45727de934ca122c8
maxisthemax <maxisthemax89@gmail.com>
2019-10-23 07:49:11 +0800

    fix

--- modals/reports/reports.js
+++ modals/reports/reports.js
@@ -1,6 +1,8 @@
 exports.queryLandsWithMpobs = function (coId,cb) {
   process.nextTick(function () {
-    var firstquery = `SELECT a.*,b.custName,c.mpobLicNo FROM lands a LEFT JOIN customers b ON a.custId = b.custId LEFT JOIN mpobs c ON FIND_IN_SET(a.landId,c.landId) > 0 where b.coId=${coId}`;
+    var firstquery = `SELECT a.*,b.custName,c.mpobLicNo FROM lands a LEFT JOIN customers b 
+    ON a.custId = b.custId LEFT JOIN mpobs c ON FIND_IN_SET(a.landId,c.landId) > 0 where b.coId=${coId} 
+    AND a.disabled=0 AND b.disabled=0`;
     //console.log(firstquery);
     con.query(firstquery, function (err, result, fields) {
       if (result) result = JSON.parse(JSON.stringify(result));
