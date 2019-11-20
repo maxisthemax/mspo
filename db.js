7b61f41b9e7fdde1e83704300968009aab8aefc7
maxisthemax <maxisthemax89@gmail.com>
2019-11-21 07:29:41 +0800

    fix

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 var config = {};
 var debug;
-debug = 1;
+debug = 0;
 
 if (debug == 1)
-config = {"host":"localhost","user":"root","password":"","database":"mspo","multipleStatements":"true","timezone": 'Z'};
+config = {"host":"localhost","user":"root","password":"","database":"mspo","multipleStatements":"true"};
 else
-config = {"host":"localhost","port":"3306","user":"max","password":"0J9_g7gb","database":"mspo","multipleStatements":"true","timezone": 'Z'};
+config = {"host":"localhost","port":"3306","user":"max","password":"0J9_g7gb","database":"mspo","multipleStatements":"true"};
 
 
 //  connect to database
