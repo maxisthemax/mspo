b6c8abe4f834509a11a3627039acd2b263a14de7
maxisthemax <maxisthemax89@gmail.com>
2019-11-21 07:34:27 +0800

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-var con = require('./db');
+var con = require('./.db');
 const dotenv = require('dotenv');
 dotenv.config();
 const Sentry = require('@sentry/node');
