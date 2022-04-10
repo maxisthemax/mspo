79dd5a7e61606d12a56db1e926867c956a9a53c9
maxisthemax <maxisthemax89@gmail.com>
2022-04-10 16:10:17 +0800

    add new thing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+var comp = require("../modals/company");
 module.exports = {
-    getHomePage: (req, res) => {
-        res.render('home.ejs', {
-            user: req.user,
-            successFlash: req.flash('success'),
-            errorFlash: req.flash('error'),
-        });
-    }
+  getHomePage: (req, res) => {
+    comp.company.queryCompany(req.user.coId, function (err, company) {
+      res.render("home.ejs", {
+        company: company,
+        user: req.user,
+        successFlash: req.flash("success"),
+        errorFlash: req.flash("error"),
+      });
+    });
+  },
 };
