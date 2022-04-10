5c13c63afefdb2f647d8a6615f5be9302d2b4018
maxisthemax <maxisthemax89@gmail.com>
2022-04-10 16:21:09 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
-var comp = require("../modals/company");
 module.exports = {
   getHomePage: (req, res) => {
-    comp.company.queryCompany(req.user.coId, function (err, company) {
-      res.render("home.ejs", {
-        company: company,
-        user: req.user,
-        successFlash: req.flash("success"),
-        errorFlash: req.flash("error"),
-      });
+    res.render("home.ejs", {
+      user: req.user,
+      successFlash: req.flash("success"),
+      errorFlash: req.flash("error"),
     });
   },
 };
